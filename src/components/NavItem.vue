df011d0596e3b30ac30dc4bf842361af55dfcc5c
asw3bd3v <asw3bd3v@gmail.com>
2023-11-28 19:30:50 +0300

    Урок 7: наследование атрибутов

---
+++ src/components/NavItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="flex-1">
+    <a
+      v-bind="$attrs"
+      class="flex flex-col items-center p-2 text-sm capitalize"
+    >
+      <slot></slot>
+    </a>
+  </li>
+</template>
+<script>
+export default {
+  // отключаем наследование атрибутов корневым элементом,
+  // т.к. применяем атрибуты на элементе <a>
+  inheritAttrs: false,
+};
+</script>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,15 +1,9 @@
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
-        <a
-          :href="`#${page}`"
-          class="flex flex-col items-center p-2 text-sm capitalize"
-        >
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-          {{ page }}
-        </a>
-      </li>
+      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItem>
     </ul>
   </nav>
 </template>
@@ -20,6 +14,7 @@ import {
   ListBulletIcon,
   ChartBarIcon,
 } from "@heroicons/vue/24/outline";
+import NavItem from "./NavItem.vue";
 
 const navItems = {
   timeline: ClockIcon,
