fdc80e9361dff727382fa4e16dd28ac0aa7c6353
asw3bd3v <asw3bd3v@gmail.com>
2023-11-28 19:44:43 +0300

    Урок 8: подсветка активного пункта меню

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,7 +1,13 @@
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="(icon, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>
@@ -9,6 +15,7 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import {
   ClockIcon,
   ListBulletIcon,
@@ -21,4 +28,6 @@ const navItems = {
   activities: ListBulletIcon,
   progress: ChartBarIcon,
 };
+
+const currentPage = ref("timeline");
 </script>
