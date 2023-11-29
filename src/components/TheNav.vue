c53879486c07fbf4478439bd02d7b93b9280771a
asw3bd3v <asw3bd3v@gmail.com>
2023-11-29 18:36:24 +0300

    Урок 10: компоненты страниц

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -6,7 +6,7 @@
         :key="page"
         :href="`#${page}`"
         :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
-        @click="currentPage = page"
+        @click="emit('navigate', page)"
       >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
@@ -15,7 +15,6 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
 import {
   ClockIcon,
   ListBulletIcon,
@@ -24,23 +23,12 @@ import {
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from "../constants.js";
 import NavItem from "./NavItem.vue";
 
+defineProps(['currentPage']);
+const emit = defineEmits(["navigate"]);
+
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon,
 };
-
-const currentPage = ref(normalizePageHash());
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1);
-
-  if (Object.keys(navItems).includes(hash)) {
-    return hash;
-  }
-
-  window.location.hash = PAGE_TIMELINE;
-
-  return PAGE_TIMELINE;
-}
 </script>
