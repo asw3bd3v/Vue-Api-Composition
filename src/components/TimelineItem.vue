2817b53128bb581c1e6c0628ea06d60ab1b0ed16
asw3bd3v <asw3bd3v@gmail.com>
2023-12-04 18:41:03 +0300

    Урок 15: компонент TimelineItem

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
+    <a href="#" :class="hourLinkClassess">{{ timelineItem.hour }}:00</a>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps(["timelineItem"]);
+
+const hourLinkClassess = [
+  "absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg",
+  props.timelineItem.hour === new Date().getHours()
+    ? "bg-purple-900 font-black text-white"
+    : "bg-gray-100 text-gray-500",
+];
+</script>
