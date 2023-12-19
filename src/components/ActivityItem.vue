6fab1324583a7a54ca58d03325b94cc986fe5255
azernov <azernov@incom-sr.ru>
2023-12-19 16:48:02 +0300

    Урок 29: типы кнопок

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,7 +1,7 @@
 <template>
 	<li class="flex flex-col gap-2 p-4">
 		<div class="flex items-center gap-2">
-			<BaseButton>
+			<BaseButton :type="BUTTON_TYPE_DANGER">
 				<TrashIcon class="h-8" />
 			</BaseButton>
 			<span class="truncate text-xl">{{ activity }}</span>
@@ -23,7 +23,7 @@ import { ref } from "vue";
 import { TrashIcon } from "@heroicons/vue/24/outline";
 import BaseSelect from "./BaseSelect.vue";
 import BaseButton from "./BaseButton.vue";
-import { PERIOD_SELECT_OPTIONS } from "../constants";
+import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from "../constants";
 import { isActivityValid } from "../validators";
 
 defineProps({
