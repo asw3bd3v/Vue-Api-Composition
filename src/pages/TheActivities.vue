dcf79efe1910d61834c99329fb5339c4b1d8d505
azernov <azernov@incom-sr.ru>
2023-12-19 16:23:03 +0300

    Урок 27: компонент активности

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,48 +1,16 @@
 <template>
-  <div>
-    <ul class="divide-y">
-      <li
-        v-for="activity in activities"
-        :key="activity"
-        class="flex flex-col gap-2 p-4"
-      >
-        <div class="flex items-center gap-2">
-          <BaseButton>
-            <TrashIcon class="h-8" />
-          </BaseButton>
-          <span class="truncate text-xl">{{ activity }}</span>
-        </div>
-        <div>
-          <BaseSelect
-            class="font-mono"
-            placeholder="h:mm"
-            :options="periodSelectOptions"
-          />
-        </div>
-      </li>
-    </ul>
-  </div>
+	<div>
+		<ul class="divide-y">
+			<ActivityItem
+				v-for="activity in activities"
+				:key="activity"
+				:activity="activity"
+			/>
+		</ul>
+	</div>
 </template>
 
 <script setup>
-import { TrashIcon } from "@heroicons/vue/24/outline";
-import BaseSelect from "../components/BaseSelect.vue";
-import BaseButton from "../components/BaseButton.vue";
-
+import ActivityItem from "../components/ActivityItem.vue";
 const activities = ["Coding", "Reading", "Training"];
-
-const periodSelectOptions = [
-  {
-    value: 15,
-    label: "0:15",
-  },
-  {
-    value: 30,
-    label: "0:30",
-  },
-  {
-    value: 45,
-    label: "0:45",
-  },
-];
 </script>
