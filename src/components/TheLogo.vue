a061a71097443a5fc55c0cacec91afcf1b0ed6b7
azernov <azernov@incom-sr.ru>
2023-12-21 13:45:28 +0300

    Урок 38: альтернативный компонент вместо пустого списка активностей

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <a :href="`#${PAGE_TIMELINE}`">
-    <img src="../assets/logo.png" alt="Logo" class="h-9" />
+    <img src="../assets/img/logo.png" alt="Logo" class="h-9" />
   </a>
 </template>
 
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<ul class="divide-y">
+	<div class="flex flex-col grow">
+		<ul v-if="activities.length" class="divide-y grow">
 			<ActivityItem
 				v-for="activity in activities"
 				:key="activity"
@@ -8,7 +8,7 @@
 				@delete="emit('deleteActivity', activity)"
 			/>
 		</ul>
-
+		<TheActivitiesEmptyState v-else />
 		<TheActivityForm @submit="emit('createActivity', $event)" />
 	</div>
 </template>
@@ -16,6 +16,7 @@
 <script setup>
 import ActivityItem from "../components/ActivityItem.vue";
 import TheActivityForm from "../components/TheActivityForm.vue";
+import TheActivitiesEmptyState from "../components/TheActivitiesEmptyState.vue";
 import { validateActivities, isActivityValid } from "../validators";
 
 const emit = defineEmits({
