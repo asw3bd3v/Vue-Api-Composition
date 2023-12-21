d9028b74917648073ff0259fa751c54fdc608423
azernov <azernov@incom-sr.ru>
2023-12-21 11:34:05 +0300

    Урок 33: форма создания активностей

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -7,9 +7,10 @@
 </template>
 
 <script>
-import { BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from "../constants";
+import { BUTTON_TYPE_PRIMARY, BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from "../constants";
 
 const typeClasses = {
+	[BUTTON_TYPE_PRIMARY]: "bg-purple-500 enabled:hover:bg-purple-600 text-white",
 	[BUTTON_TYPE_DANGER]: "bg-red-500 enabled:hover:bg-red-600 text-white",
 	[BUTTON_TYPE_NEUTRAL]: "bg-gray-100 enabled:hover:bg-gray-200",
 };
@@ -22,7 +23,7 @@ import { isButtonTypeValid } from "../validators";
 defineProps({
 	type: {
 		type: String,
-		default: BUTTON_TYPE_NEUTRAL,
+		default: BUTTON_TYPE_PRIMARY,
 		validator: isButtonTypeValid,
 	},
 });
--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,51 +1,56 @@
 <template>
-  <div class="flex gap-2">
-    <BaseButton @click="emit('select', null)">
-      <XMarkIcon class="h-8" />
-    </BaseButton>
-    <select
-      class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl"
-      @change="emit('select', +$event.target.value)"
-    >
-      <option :selected="isNotSelected" disabled value="">
-        {{ placeholder }}
-      </option>
-      <option
-        v-for="{ value, label } in options"
-        :key="value"
-        :value="value"
-        :selected="value === selected"
-      >
-        {{ label }}
-      </option>
-    </select>
-  </div>
+	<div class="flex gap-2">
+		<BaseButton @click="emit('select', null)" :type="BUTTON_TYPE_NEUTRAL">
+			<XMarkIcon class="h-8" />
+		</BaseButton>
+		<select
+			class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl"
+			@change="emit('select', +$event.target.value)"
+		>
+			<option :selected="isNotSelected" disabled value="">
+				{{ placeholder }}
+			</option>
+			<option
+				v-for="{ value, label } in options"
+				:key="value"
+				:value="value"
+				:selected="value === selected"
+			>
+				{{ label }}
+			</option>
+		</select>
+	</div>
 </template>
 
 <script setup>
 import { computed } from "vue";
 import BaseButton from "./BaseButton.vue";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
-import { validateSelectOptions, isUndefinedOrNull, isNumberOrNull } from "../validators";
+import {
+	validateSelectOptions,
+	isUndefinedOrNull,
+	isNumberOrNull,
+} from "../validators";
+import { BUTTON_TYPE_NEUTRAL } from "../constants";
 
 const props = defineProps({
-  selected: Number,
-  placeholder: {
-    type: String,
-    required: true,
-  },
-  options: {
-    type: Array,
-    required: true,
-    validator: validateSelectOptions,
-  },
+	selected: Number,
+	placeholder: {
+		type: String,
+		required: true,
+	},
+	options: {
+		type: Array,
+		required: true,
+		validator: validateSelectOptions,
+	},
 });
 
 const emit = defineEmits({
-  /* select(value) {
+	/* select(value) {
     return typeof value === "number";
   }, */
-  select: isNumberOrNull
+	select: isNumberOrNull,
 });
 
 const isNotSelected = computed(() => isUndefinedOrNull(props.selected));
