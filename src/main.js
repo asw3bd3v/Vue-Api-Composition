61a6d10898b79912ee97c8f4ccfdfc31639aa504
azernov <azernov@incom-sr.ru>
2024-02-12 16:03:38 +0300

    Урок 118: сохранение состояния приложения в локальном хранилище

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+import * as storage from './storage.js';
+import { activities } from './activities.js';
+import { timelineItems } from './timeline-items.js';
 import './style.css'
 import App from './App.vue'
+
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'hidden') {
+        saveState();
+    }
+});
+
+function saveState() {
+    storage.save({
+        timelineItems: timelineItems.value,
+        activities: activities.value,
+    });
+}
 
 createApp(App).mount('#app')
