58d58bcc8c46de57844f918ad480c70154288a8d
azernov <azernov@incom-sr.ru>
2024-02-12 16:14:10 +0300

    Урок 119: загрузка состояния приложения из локального хранилища

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import * as storage from './storage.js';
 import { activities } from './activities.js';
 import { timelineItems } from './timeline-items.js';
-import './style.css'
-import App from './App.vue'
+import './style.css';
+import App from './App.vue';
+
+loadState();
 
 document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'hidden') {
-        saveState();
-    }
+    document.visibilityState === 'visible'
+        ? loadState()
+        : saveState();
 });
 
 function saveState() {
@@ -16,6 +18,13 @@ function saveState() {
         timelineItems: timelineItems.value,
         activities: activities.value,
     });
+}
+
+function loadState() {
+    const state = storage.load();
+
+    timelineItems.value = state.timelineItems;
+    activities.value = state.activities;
 }
 
 createApp(App).mount('#app')
